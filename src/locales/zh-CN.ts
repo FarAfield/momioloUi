a62dc3dbff5576b5ecc24b3299e96fe52cda9c00
0027017199 <chen.wu38@iwhalecloud.com>
2020-11-16 20:32:14 +0800

    增加图标logo

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -1,6 +1,6 @@
 import settings from './zh-CN/settings';
 export default {
-  'momiolo-login': 'momiolo-登陆',
+  'momiolo-login': '登录-momiolo',
   'momiolo-title': 'Momiolo',
   'momiolo-subTitle': 'Momiolo 是西湖区最具影响力的 Web 设计规范',
   ...settings,
