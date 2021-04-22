08206bb420cef3abfd261b019ad229269caadf93
0027017199 <chen.wu38@iwhalecloud.com>
2021-04-22 09:31:28 +0800

    添加异步并发限制方法

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import { ThemeConfig } from '@/utils/constant';
+import moment from 'moment';
 
 /**===========================  Token   ==================================*/
 export function isLogin() {
@@ -37,3 +38,41 @@ export const changeTheme = (primaryColor: any = '#1890ff') => {
     }
   }
 };
+
+/**=======================   异步并发限制   =============================*/
+// poolLimit：并发限制  array  iteratorFn: 要执行的函数（返回promise）
+export async function asyncPool(poolLimit: number, array: Array<any>, iteratorFn: Function) {
+  const ret = [];
+  const executing: Array<any> = []; // 当前正在执行的Promise数组
+  for (const item of array) {
+    // 实例化promise
+    const p = Promise.resolve().then(() => iteratorFn(item, array));
+    ret.push(p);
+    const e: any = p.then(() => executing.splice(executing.indexOf(e), 1));
+    executing.push(e);
+    if (executing.length >= poolLimit) {
+      await Promise.race(executing);
+    }
+  }
+  return Promise.all(ret);
+}
+/**使用方式如下*/
+// 10个随机数的数组
+const array = new Array(10).fill(true).map((i) => {
+  return Math.ceil(Math.random() * 20);
+});
+// 入参为数组的每一项，返回一个promise
+function iteratorFn(delay: any) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(delay, moment().format('m分s秒'));
+      resolve(delay);
+    }, delay * 1000);
+  });
+}
+// 使用异步并发，确保大量请求时线程池每次都只有2个promise在执行
+// @ts-ignore
+async function start() {
+  const results = await asyncPool(2, array, iteratorFn);
+  console.log(results, '结果');
+}
