ccaa6ecb08c016ed9aa7723c093ebb598fc34ffb
陈武 <1306594079@qq.com>
2021-05-12 15:24:09 +0800

    请求中断方法

--- src/services/base.ts
+++ src/services/base.ts
@@ -2,6 +2,7 @@ import request from '@/utils/request';
 
 interface Payload {
   url: string;
+  signal: any;
   params?: object;
   list?: any[];
   file?: any;
@@ -9,70 +10,76 @@ interface Payload {
 /**
  *   get方法
  */
-export async function getData({ url, ...params }: Payload) {
+export async function getData({ url, signal, ...params }: Payload) {
   return request.get(`${url}`, {
+    signal,
     params,
   });
 }
 /**
  *  post方法
  */
-export async function postData({ url, ...params }: Payload) {
+export async function postData({ url, signal, ...params }: Payload) {
   return request.post(`${url}`, {
+    signal,
     data: params,
   });
 }
 /**
  *  post方法
  */
-export async function postParams({ url, ...params }: Payload) {
+export async function postParams({ url, signal, ...params }: Payload) {
   return request.post(`${url}`, {
+    signal,
     params,
   });
 }
 /**
  *  post方法
  */
-export async function postList({ url, list }: Payload) {
+export async function postList({ url, signal, list }: Payload) {
   return request.post(`${url}`, {
+    signal,
     data: list,
   });
 }
 /**
  *  put方法
  */
-export async function putData({ url, ...params }: Payload) {
+export async function putData({ url, signal, ...params }: Payload) {
   return request.put(`${url}`, {
+    signal,
     data: params,
   });
 }
 /**
  *  put方法
  */
-export async function putParams({ url, ...params }: Payload) {
+export async function putParams({ url, signal, ...params }: Payload) {
   return request.put(`${url}`, {
+    signal,
     params,
   });
 }
 /**
  *  put方法
  */
-export async function putList({ url, list }: Payload) {
+export async function putList({ url, signal, list }: Payload) {
   return request.put(`${url}`, {
+    signal,
     data: list,
   });
 }
 /**
  *  delete方法
  */
-export async function remove({ url,...params }: Payload) {
-  return request.delete(`${url}`, { params });
+export async function remove({ url, signal, ...params }: Payload) {
+  return request.delete(`${url}`, { signal, params });
 }
-
 
 /**
  *  upload方法
  */
-export async function upload({ url, file }: Payload) {
-  return request.post(`${url}`, { data: file });
+export async function upload({ url, signal, file }: Payload) {
+  return request.post(`${url}`, { signal, data: file });
 }
--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -13,3 +13,24 @@ export const useCurrentTime = () => {
   }, []);
   return dayjs.unix(time).format('YYYY-MM-DD HH:mm:ss');
 };
+
+/**
+ *  获取一个请求信号
+ *  与请求相关联，则可以使用cancel方法取消请求
+ *  例如：
+ *  const [signal,cancel] = useSignal()
+ *  dispatch({
+ *    type:'base/getData',
+ *    payload: { url:"xxx", signal }
+ *  })
+ *  使用cancel则可以取消掉这次请求
+ */
+export const useSignal = () => {
+  const controller = new AbortController(); // 创建一个控制器
+  const { signal } = controller; // 返回一个 AbortSignal 对象实例，它可以用来 with/abort 一个 DOM 请求
+  const [value] = useState<any>(signal);
+  function cancel() {
+    controller.abort();
+  }
+  return [value, cancel];
+};
