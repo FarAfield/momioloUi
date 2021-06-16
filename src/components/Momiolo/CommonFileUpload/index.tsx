0b42a023e64fd9acf63c93e6cb03d6e7aaa3086a
陈武 <1306594079@qq.com>
2021-06-16 18:04:07 +0800

    文件上传组件扩展图片上传

--- src/components/Momiolo/CommonFileUpload/index.tsx
+++ src/components/Momiolo/CommonFileUpload/index.tsx
@@ -1,29 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Upload, Button, message } from 'antd';
 import { getToken } from '@/utils/utils';
 
 /**
- *  扩展  limit(限制上传文件数量)
- *        title(按钮显示文字)
- *
- *
- *
- * @param props
- * @constructor
+ *  默认为文件上传  uploadProps中可设置listType为text || picture
+ *  若使用图片上传，必须设置uploadUrl以及设置uploadProps中listType为picture-card
  */
 
 const CommonFileUpload = (props: CommonFileUploadProps) => {
-  const { fileMaxSize, suffix, customFileCheck, value, onChange, uploadUrl, uploadProps } = props;
+  const {
+    fileMaxSize,
+    suffix,
+    customFileCheck,
+    limit,
+    title,
+    value,
+    onChange,
+    uploadUrl,
+    uploadProps,
+  } = props;
   const [fileList, setFileList] = useState<Array<any>>([]); // 组件内部fileList
   const [fileResult, setFileResult] = useState<Array<any>>([]);
+  const isImg = useMemo(() => {
+    return uploadProps?.listType && uploadProps?.listType.includes('picture');
+  }, [uploadProps]);
 
   function convertToFileList(urlList: any) {
     return urlList.map((i: any, index: number) => {
+      const extra = isImg ? { url: i.fileUrl, thumbUrl: i.fileUrl } : {};
       return {
         uid: Number(`-${index + 1}`),
         name: i.fileName || i.fileUrl,
         status: 'done',
         response: { data: { fileUrl: i.fileUrl } },
+        ...extra,
       };
     });
   }
@@ -41,7 +51,6 @@ const CommonFileUpload = (props: CommonFileUploadProps) => {
       return Upload.LIST_IGNORE;
     }
     // 文件大小校验
-    // @ts-ignore
     if (file.size / 1024 / 1024 > fileMaxSize) {
       message.error(`文件大小不能大于${fileMaxSize}MB！`);
       return Upload.LIST_IGNORE;
@@ -55,11 +64,22 @@ const CommonFileUpload = (props: CommonFileUploadProps) => {
 
   function onFileChange(info: any) {
     const { fileList, file } = info;
-    const { name, status, response } = file;
+    const { uid, name, status, response } = file;
     const { statusCode, statusMessage, data } = response || {};
     setFileList(fileList);
     if (status === 'done' && statusCode === '0' && data) {
       message.success(`${name} 上传成功`);
+      // 对文件增加url以及thumbUrl属性（图片使用）
+      const cloneFileList = [...fileList];
+      const index = cloneFileList.findIndex((i: any) => i.uid === uid);
+      cloneFileList[index] = {
+        ...cloneFileList[index],
+        url: data?.fileUrl,
+        thumbUrl: data?.fileUrl,
+      };
+      if(isImg){
+        setFileList(cloneFileList);
+      }
       setFileResult(
         fileResult.concat({
           fileName: name,
@@ -97,22 +117,29 @@ const CommonFileUpload = (props: CommonFileUploadProps) => {
       onChange={onFileChange}
       {...uploadProps}
     >
-      <Button>上传文件</Button>
+      {fileList.length < limit && (
+        <>{uploadProps?.listType === 'picture-card' ? `${title}` : <Button>{`${title}`}</Button>}</>
+      )}
     </Upload>
   );
 };
 CommonFileUpload.defaultProps = {
   fileMaxSize: 10, // 10M
   suffix: '',
+  limit: 5,
+  title: '上传文件',
   value: [],
-  uploadUrl: '/base/fileUpload/commonUpload',
+  uploadUrl: '/base/fileUpload/commonFileUpload', // commonFileUpload  commonImgUpload
+  uploadProps: {},
 };
 export default CommonFileUpload;
 
 interface CommonFileUploadProps {
-  fileMaxSize?: number;
+  fileMaxSize?: any;
   suffix?: string;
-  customFileCheck?: Function;
+  customFileCheck?: Function; // 自定义文件上传前校验
+  limit?: any;
+  title?: string;
   value?: Array<fileResultProps>;
   onChange?: Function;
   uploadUrl?: string;
